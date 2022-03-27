216f2543e3427055ecce7aae6660fa70815e52ad
Eric Xue <widefin@gmail.com>
2022-03-27 14:32:30 -0400

    2

--- usaco/contest/2022/2022_us_open_silver_2.py
+++ usaco/contest/2022/2022_us_open_silver_2.py
@@ -1,42 +1,43 @@
 import sys
+from collections import deque
 
-def check_twol(s, t, a, b):
-    p1 = 0
-    p2 = 0
-    while p1 < len(a) and p2 < len(b):
-        if s[p1] != a and s[p1] != b:
-            p1 += 1
-        if t[p2] != a and t[p2] != b:
-            p2 += 1
-        if s[p1] == a or s[p1] == b and t[p2] == a or t[p2] == b:
-            if s[p1] != t[p2]:
-                return False
-            p1 += 1
-            p2 += 1
-    return True
+def check_query(s, t, q):
+    s1 = []
+    s2 = []
+    for ch in s:
+        if ch in q:
+            s1.append(ch)
 
-def check_query(s, t, q, twol_map):
-    for i in range(1, len(q)):
-        if not twol_map[(q[i], q[i-1])]:
-            return False
-    return True
+    for ch in t:
+        if ch in q:
+            s2.append(ch)
 
+    if len(s1) != len(s2):
+        return False
+
+    return len(s1)>0 and s1==s2
 
 s = sys.stdin.readline()[:-1]
 t = sys.stdin.readline()[:-1]
 
-twol_map = {}
-for a in range(97, 115): # letter a to r
-    for b in range(97, 115):
-        twol_map[(chr(a), chr(b))] = check_twol(s, t, chr(a), chr(b))
+pool = list("abcdefghijklmnopqr")
+comb_queue = deque(pool)
+valid_queries = set()
 
-for k in twol_map:
-    print(k, twol_map[k])
+while len(comb_queue):
+    q = comb_queue.popleft()
+    if check_query(s, t, q):
+        valid_queries.add(q)
+        for ch in pool:
+            if ch not in q:
+                comb_queue.append("".join(sorted(q + ch)));
 
+print('here')
+
+#solve
 queries = int(sys.stdin.readline())
 for _ in range(queries):
-    q = sys.stdin.readline()[:-1]
-    if check_query(s, t, q, twol_map):
+    if sys.stdin.readline()[:-1] in valid_queries:
         print('Y', end='')
     else:
         print('N', end='')
