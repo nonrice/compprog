bbe0e4e93199ea63d26374db0e175b07bf8a767e
Eric Xue <widefin@gmail.com>
2022-03-28 21:35:35 -0400

    bruh

--- codeforces/1400/aliceAndCake.cpp
+++ codeforces/1400/aliceAndCake.cpp
@@ -1,34 +1,44 @@
 #include <iostream>
-#include <queue>
 #include <numeric>
-#include <unordered_set>
+#include <queue>
+#include <unordered_map>
 
 #define ll long long
 
 void solve(int* nums, int n){
-    ll sum = std::accumulate(nums, nums+n, 0);
-    std::queue<ll> q({sum});
-    std::unordered_set<ll> valid_weights({sum});
-    
-    ll a;
-    while (q.size()){
-        a = q.front(); q.pop();
-        valid_weights.insert(a/2);
-        valid_weights.insert((a+1)/2);
-        if (a!=1 && !valid_weights.count(a)){
-            q.push(a/2);
-            q.push((a+1)/2);
-        }
-    }
+    std::unordered_map<int, int> freq;
+    for (int i=0; i<n; ++i)
+        freq[nums[i]]++;
+    int to_use = freq.size();
 
-    for (int i=0; i<n; ++i){
-        if (!valid_weights.count(nums[i])){
-            std::cout << "NO\n";
-            return;
+    std::queue<int> q({std::accumulate(nums, nums+n, 0)});
+    ll a;
+    while (!q.empty()){
+        a = q.front(); q.pop();
+        if (!freq.count(a)){
+            if (a > 1){
+                q.push(a/2);
+                q.push((a+1)/2);
+            }
+        } else {
+            if (freq[a] > 0){
+                if (freq[a]-- == 0){
+                    if (to_use-- == 0)
+                        std::cout << "YES\n";
+                }
+            } else {
+                std::cout << "NO\n";
+                return;
+            }
         }
     }
     
-    std::cout << "YES\n";
+    if (to_use == 0){
+        std::cout << "YES\n";
+    } else {
+        std::cout << "NO\n";
+        std::cout << "asdf";
+    }
     return;
 }
 
