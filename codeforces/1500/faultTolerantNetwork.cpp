dc7c7ea53f655b45edd689119e841328417a0f12
Eric Xue <widefin@gmail.com>
2022-04-11 12:03:52 -0400

    Update faultTolerantNetwork.cpp

--- codeforces/1500/faultTolerantNetwork.cpp
+++ codeforces/1500/faultTolerantNetwork.cpp
@@ -4,39 +4,32 @@
 
 #define ll long long
 
-int calc_nearest(int x, int* arr, int n){
+int get_nearest(int x, int* arr, int n){
     int min_diff = 1000000001;
-    int val = arr[0];
+    int p = 0;
     for (int i=0; i<n; ++i){
         if (std::abs(x-arr[i]) < min_diff){
             min_diff = std::abs(x-arr[i]);
-            val = arr[i];
+            p = i;
         }
     }
-    return val;
+    return p;
 }
 
 int solve(int* a, int* b, int n){
-    ll sa=a[0], ea=a[n-1], sb=b[0], eb=b[n-1];
-    std::sort(a, a+n);
-    std::sort(b, b+n);
-    ll nsa = calc_nearest(sa, b, n);
-    ll nea = calc_nearest(ea, b, n);
-    ll nsb = calc_nearest(sb, a, n);
-    ll neb = calc_nearest(eb, a, n);
-    ll cost = 0;
-    if (nsa == sb || nsb == sa){
-        cost += std::abs(sa-sb);
-    } else {
-        cost += std::abs(sa-nsa) + std::abs(sb-nsb);
+    ll min = 10000000000LL;
+    for (int v1:{0, get_nearest(a[0], b, n), n-1}){
+        for (int v2:{0, get_nearest(a[n-1], b, n), n-1}){
+            ll can = (ll)std::abs(a[0]-b[v1]) + std::abs(a[n-1]-b[v2]);
+            if (v1>0 && v2>0)
+				can += std::abs(b[0]-a[get_nearest(b[0], a, n)]);
+		    if (v1<n-1 && v2<n-1)
+				can += std::abs(b[n-1]-a[get_nearest(b[n-1], a, n)]);
+			min = std::min(min, can);
+        }
     }
-
-    if (nea == eb || neb == ea){
-        cost += std::abs(ea-eb);
-    } else {
-        cost += std::abs(ea-nea) + std::abs(eb-neb);
-    }
-    return cost;
+    
+    return min;
 }
 
 int main(){
