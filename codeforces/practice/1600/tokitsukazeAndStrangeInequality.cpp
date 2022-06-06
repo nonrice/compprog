c4bcef95e5fd2b39151d6f58b03751cb9983a3b7
Eric Xue <widefin@gmail.com>
2022-06-06 17:19:38 -0400

    good

--- codeforces/practice/1600/tokitsukazeAndStrangeInequality.cpp
+++ codeforces/practice/1600/tokitsukazeAndStrangeInequality.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+using ll = long long;
+
 void solve(){
     int n; cin >> n;
     vector<int> p(n);
@@ -10,14 +12,26 @@ void solve(){
     for (int b=1; b<=n; ++b){
         for (int d=b+1; d<=n; ++d){
             pr[b][d] = pr[b-1][d] + pr[b][d-1] - pr[b-1][d-1] + (p[b-1]>p[d-1]);
+            //pr[b][d] = pr[b][d-1] + (p[b-1]>p[d-1]);
         }
     }
     
-    long long ans = 0;
+    /*
+    for (auto& a : pr){
+        for (int b : a){
+            cout << b << ' ';
+        }
+        cout << '\n';
+    }
+    */
+
+    ll ans = 0;
     for (int a=0; a<n; ++a){
         for (int c=a+1; c<n; ++c){
             if (p[a] < p[c])
                 ans += pr[c][n] - pr[c][c+1] - pr[a+1][n] + pr[a+1][c+1];
+            // a+1 and c+1
+            //cout << a+1 << ' ' << c+1 << ':' << pr[c-1][n] - pr[c-1][c] - pr[a][n] + pr[a][c] << '\n';
         }
     }
     
