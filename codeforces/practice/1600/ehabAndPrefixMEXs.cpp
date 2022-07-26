7b8947f2ee6777b9092a549dd4e9c7d55d1e720e
Eric Xue <widefin@gmail.com>
2022-07-25 21:25:50 -0400

    totally did not read the editorial for that

---
+++ codeforces/practice/1600/ehabAndPrefixMEXs.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n; cin >> n;
+    vector<int> a(n);
+    vector<bool> c(n+1);
+    for (int& v : a){
+        cin >> v;
+        c[v] = true;
+    }
+    
+    vector<int> b(n, -1);
+    for (int i=1; i<n; ++i){
+        if (a[i] != a[i-1]) b[i] = a[i-1];
+    }
+    
+    int p=0;
+    for (int& v : b){
+        if (v == -1){
+            while (c[p]) ++p;
+            c[p] = 1;
+            v = p;
+        }
+
+        cout << v << ' ';
+    }
+    
+    cout << '\n';
+}
